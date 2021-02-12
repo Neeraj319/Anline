9482e6c52ca205b2d7c7fb807e3e643ed7e9316c
Neeraj <itskop520@gmail.com>
2021-02-12 17:14:08 +0545

    many changes made

--- Auth_users/views.py
+++ Auth_users/views.py
@@ -1,15 +1,60 @@
 # Create your views here.
-from django.shortcuts import render, HttpResponse
-
-
+from django.shortcuts import render, HttpResponse , redirect
+from django.contrib import messages
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.models import User, auth
+from .models import Buyer
 # Create your views here.
 def login_user(request):
-    return HttpResponse('login page')
+    if request.user.is_anonymous:
+        if request.method == 'POST':
+            user_name = request.POST.get('user_name')
+            password = request.POST.get('password')
+            user = authenticate(username=user_name, password=password)
+            if user is not None:
+
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(
+                    request, 'user name or password did not matched')
+                return redirect('login')
+    else:
+
+        return redirect('home')
+    return render(request , 'login.html')
 
 
 def register_user(request):
-    return HttpResponse("register page")
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        address = request.POST.get('address')
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'username already exists')
+        elif User.objects.filter(email = email).exists():
+            messages.error(request, 'eamil already exists')
+        elif (len(password) and len(password2)) <= 7:
+            messages.error(request, 'password is too short')
+        elif password != password2:
+            messages.error(request, 'first password did not matched with another')
+        else:
+            First_name = first_name.capitalize()
+            Last_name = last_name.capitalize()
+            user = User.objects.create_user(
+                    username=username, password=password2, first_name=First_name, last_name=Last_name, email=email)
+            user.save()
+            Buyer.objects.create(user = user , address=address)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    return render(request , 'register.html')
 
 
 def logout_user(request):
-    return HttpResponse("logout page")
+    logout(request)
+    return redirect('login')
--- ec/settings.py
+++ ec/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import dotenv
+import os
+dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,8 +130,8 @@ LOGOUT_URL = 'logout'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'your email'
-EMAIL_HOST_PASSWORD = 'your email password'
+EMAIL_HOST_USER = os.environ['email']
+EMAIL_HOST_PASSWORD = os.environ['password']
 EMAIL_USE_TLS = True
 STATICFILES_DIRS = [
     BASE_DIR / "static",
